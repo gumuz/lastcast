316c2a7eba6afc1c5fcc9ab1081566868a6094b2
guyon.moree <guyon.moree@82481a36-8fc1-11dd-9171-a90e9d83591c>
2008-10-01 18:26:36 +0000

    more reorg....

--- trunk/lastcast.py
+++ trunk/lastcast.py
@@ -11,14 +11,14 @@ popen2.popen2("pyrcc4 ./ui/lastcast.qrc > ./ui/lastcast_rc.py")
 popen2.popen2("pyuic4 ./ui/lastcast_main.ui > ./ui/lastcast_main_ui.py")
 popen2.popen2("pyuic4 ./ui/lastcast_stations.ui > ./ui/lastcast_stations_ui.py")
 
-from ui.lastcastmain import MainWindow
+from ui.lastcast_main import LastcastMainWindow
 
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     app.setStyle('cleanlooks')
 
-    window = MainWindow()
+    window = LastcastMainWindow()
     window.show()
 
     sys.exit(app.exec_())
